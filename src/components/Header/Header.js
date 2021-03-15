2bb473d32093b659c71725c6068ebfdbd918360a
Subraiz <subraizahmed@gmail.com>
2021-03-15 15:47:56 -0400

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,13 @@ const Header = (props) => {
   return (
     <div className={styles.headerContainer}>
       <IoCameraOutline className={[styles.icon, styles.camera].join(" ")} />
-      <img src={"/images/logo.png"} alt="logo" className={styles.logo} />
+      <img
+        src={
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
+        }
+        alt="logo"
+        className={styles.logo}
+      />
       <FiSend className={[styles.icon, styles.send].join(" ")} />
     </div>
   );
